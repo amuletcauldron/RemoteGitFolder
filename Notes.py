7414af651c585d791bdca77e546256291b78f8eb
Leo Mokriski <leo.mokriski@emory.edu>
2024-09-06 11:16:46 -0400

    Testing commit system

---
+++ Notes.py
@@ -0,0 +1,28 @@
+v = 10^2
+print(v)
+print(pow(8,2))
+
+# data types  in python
+
+n = 7 # integer
+r = 3. # real number
+
+print(type(n), type(r)) # prints data type of variables i.e. int & float
+sure = False # Boolean variable
+
+print("Does 1 + 1 = 3? ", sure) # displays false
+print(type(sure))
+
+name = "Leo" # string 
+lastname = "Mokriski" # string
+print(name + " " + lastname) # + " " + adds a space, or add space in string variable
+
+
+print(5**2) # powers
+print(2*lastname)
+
+nr = float(n) # casting an integer into a float explicitly (not necessary)
+number = "3"
+integer = int(number) # converts number into an actual integer
+
+
